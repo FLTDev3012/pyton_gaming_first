6398514cf067e11d990935daf26c6e4d608283b6
dorian fiolet <dorian3012@hotmail.fr>
2024-11-07 21:36:00 +1000

    creation de l ecran

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ pygame.init()
 
 # generer la fenetre de notre jeux
 pygame.display.set_caption("Comet fall Game")
-screen = pygame.display.set_mode((500, 300))
+screen = pygame.display.set_mode((1080, 720))
+
+# importer et charger l'arriere plan de notre jeu
+background = pygame.image.load('assets/bg.jpg')
 
 running = True
 
 # boucle tant que cette condition est vrai
 while running:
+
+    # appliquer l'arriere plan de notre jeu
+    screen.blit(background, (0, -200))
+
+    # mettre a jour l'ecran
+    pygame.display.flip()
 
     # si le joueur ferme cette fenetre
     for event in pygame.event.get():
