fbe2f8561c17fe9b265aee4cd8f9a22b900788c3
dorian fiolet <dorian3012@hotmail.fr>
2024-11-25 19:01:50 +1000

    save

--- animation.py
+++ animation.py
@@ -9,19 +9,35 @@ class AnimateSprite(pygame.sprite.Sprite):
         self.image = pygame.image.load(f'assets/{sprite_name}.png')
         self.current_image = 0 # commencer l'animation à l'image 0
         self.images = animations.get(sprite_name)
+        self.animation = False
+
+    # definir une methode pour demarrer l'animation
+    def start_animation(self):
+        self.animation = True
 
     # definir une methode pour animer le sprite
-    def animate(self):
-        # passer à l'image suivante
-        self.current_image += 1
+    def animate(self, loop=False):
 
-        # verifier si l'animation est arrivee à la fin, et la recommencer
-        if self.current_image >= len(self.images):
-            # remettre l'animation au debut
-            self.current_image = 0
+        # verifier si l'animation est active
+        if self.animation:
 
-        # modifier l'image précédente par la suivante
-        self.image = self.images[self.current_image]
+            # passer à l'image suivante
+            self.current_image += 1
+
+            # verifier si l'animation est arrivee à la fin, et la recommencer
+            if self.current_image >= len(self.images):
+
+                # remettre l'animation au debut
+                self.current_image = 0
+
+                # verifier si l'animation n'est pas en mode boucle
+                if not loop:
+
+                    # desactiver l'animation
+                    self.animation = False
+
+            # modifier l'image précédente par la suivante
+            self.image = self.images[self.current_image]
 
 # definir une fonction pour charger les images d'un sprite
 def load_animation_images(sprite_name):
@@ -41,6 +57,8 @@ def load_animation_images(sprite_name):
 
 # definir un dictioonaire qui va contenir les images chargees de chaque sprite
 # mummy -> [...mummy1.png, ...mummy2.png, ...]
+# player -> [...player1.png, ...player2.png, ...]
 animations = {
-    'mummy': load_animation_images('mummy')
+    'mummy': load_animation_images('mummy'),
+    'player': load_animation_images('player')
 }
--- player.py
+++ player.py
@@ -1,18 +1,18 @@
 import pygame
 from projectile import Projectile
+import animation
 
 # creer une nouvelle classe qui va representer notre joueur
-class Player(pygame.sprite.Sprite):
+class Player(animation.AnimateSprite):
 
     def __init__(self, game):
-        super().__init__()
+        super().__init__('player')
         self.game = game
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
         self.all_projectiles = pygame.sprite.Group()
-        self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 500
@@ -24,8 +24,8 @@ class Player(pygame.sprite.Sprite):
             # si le joueur n'a plus de points de vie
             self.game.game_over()
 
-
-
+    def update_animation(self):
+        self.animate()
 
 
     def update_health_bar(self, surface):
@@ -40,6 +40,8 @@ class Player(pygame.sprite.Sprite):
     def launch_projectile(self):
         # creer une nouvelle instance de la classe projectile
         self.all_projectiles.add(Projectile(self))
+        # demarrer l'animation du lancer
+        self.start_animation()
 
     def move_right(self):
         # si le joueur n'est pas en collision avec un monstre
